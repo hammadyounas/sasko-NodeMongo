f2ad0e58fa9827fc7a7e5cb1730dd73580a20f6e
Taimoor Tariq <taimoor_t@yahoo.com>
2019-09-11 19:21:55 +0500

    duplciate item name remove

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -1,46 +1,63 @@
 const Items = require('../models/item.model')
 
 module.exports.getItems = (req, res) => {
-  console.log("get");
+  console.log('get')
 
-  Items.find().then(Items => {
-    res.send(Items)
-  }).catch(error => {
-    res.send(error)
-  })
+  Items.find()
+    .then(Items => {
+      res.send(Items)
+    })
+    .catch(error => {
+      res.send(error)
+    })
 }
 
 module.exports.deleteItems = (req, res) => {
-
-  Items.remove({ _id: req.params.id }).then(Items => {
-    res.send(Items)
-  }).catch(error => {
-    res.send(error)
-  })
+  Items.remove({ _id: req.params.id })
+    .then(Items => {
+      res.send(Items)
+    })
+    .catch(error => {
+      res.send(error)
+    })
 }
 
-
 module.exports.editItems = (req, res) => {
-
-  Items.findByIdAndUpdate({ _id: req.body._id }, { name: req.body.name },{new: true}).exec((error, doc) => {
-    if (error)
-      res.send(error);
+  Items.findByIdAndUpdate(
+    { _id: req.body._id },
+    { name: req.body.name },
+    { new: true }
+  ).exec((error, doc) => {
+    if (error) res.send(error)
 
     res.send(doc)
   })
-
 }
 
-
-
 module.exports.addItems = (req, res) => {
-  Items.create(req.body).then(function (ninja) {
-    res.send(ninja)
-  }).catch(error => {
-    res.send(500).json({
-      stack: error.stack,
-      code: error.code,
-      message: error.message
+  Items.findOne({ name: req.body.name })
+    .then(result => {
+      if (!result) {
+        Items.create(req.body)
+          .then(function (ninja) {
+            res.send(ninja)
+          })
+          .catch(error => {
+            res.send(500).json({
+              stack: error.stack,
+              code: error.code,
+              message: error.message
+            })
+          })
+      } else {
+        res.status(409).send({ msg: 'item already exist' })
+      }
     })
-  })
+    .catch(error => {
+      res.send(500).json({
+        stack: error.stack,
+        code: error.code,
+        message: error.message
+      })
+    })
 }
