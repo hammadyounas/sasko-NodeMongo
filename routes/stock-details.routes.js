14f0493e6b09789a5c64eaec54dbc4194b85eef3
Taimoor Tariq <taimoor_t@yahoo.com>
2019-09-19 15:15:20 +0500

    stock detail route set

--- routes/stock-details.routes.js
+++ routes/stock-details.routes.js
@@ -6,7 +6,7 @@ const stockDetails = require('../controllers/stock-details.controller')
 
 console.log("Stock-Details Route Loaded");
 
-routes.get('/getStockDetails', stockDetails.getStockDetails);
+routes.get('/', stockDetails.getStockDetails);
 routes.post('/', stockDetails.addStockDetailsWithStock);
 routes.delete('/:id', stockDetails.deleteStockDetails);
 routes.put('/', stockDetails.editStockDetails);
