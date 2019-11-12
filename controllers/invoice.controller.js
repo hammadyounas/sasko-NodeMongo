fcae291c2347a1067eaaa7ab38d6e601d11dcd74
hammadyounas <m.hammadyounas@outlook.com>
2019-11-12 16:31:33 +0500

    invoice auth handle

--- controllers/invoice.controller.js
+++ controllers/invoice.controller.js
@@ -6,108 +6,180 @@ const getInvoiceNumber = require('../utils/invoiceNumberGenerator')
 const sixDigits = require('../utils/sixDigits')
 
 module.exports.getInvoice = (req, res) => {
-  Invoice.find({ status: true })
-    .populate('customerId', 'clientName')
-    .then(invoices => {
-      if (!invoices.length) {
-        res.status(404).send({ message: 'No Data Found' })
-      } else {
-        res.status(200).send(invoices)
-      }
-    })
-    .catch(err => {
-      res.status(500).json(errorHandler(err))
-    })
+  jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (
+    err,
+    payload
+  ) {
+    if (err) {
+      res.send(401).send({ message: 'not authentic user' })
+    } else {
+      Invoice.find({ status: true })
+        .populate('customerId', 'clientName')
+        .then(invoices => {
+          if (!invoices.length) {
+            res.status(404).send({ message: 'No Data Found' })
+          } else {
+            res.status(200).send(invoices)
+          }
+        })
+        .catch(err => {
+          res.status(500).json(errorHandler(err))
+        })
+    }
+  })
 }
 
 module.exports.getInvoiceId = (req, res) => {
-  Invoice.count()
-    .then(length => {
-      let id = sixDigits((length + 1).toString())
-      res.status(200).send({ invoiceId: id })
-    })
-    .catch(err => {
-      res.status(500).json(errorHandler(err))
-    })
+  jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (
+    err,
+    payload
+  ) {
+    if (err) {
+      res.send(401).send({ message: 'not authentic user' })
+    } else {
+      Invoice.count()
+        .then(length => {
+          let id = sixDigits((length + 1).toString())
+          res.status(200).send({ invoiceId: id })
+        })
+        .catch(err => {
+          res.status(500).json(errorHandler(err))
+        })
+    }
+  })
 }
 
 module.exports.getInvoiceById = (req, res) => {
-  Invoice.findById({ _id: req.params.id, status: true })
-    .then(invoice => {
-      res.status(200).send(invoice)
-    })
-    .catch(err => {
-      res.status(500).json(errorHandler(err))
-    })
+  jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (
+    err,
+    payload
+  ) {
+    if (err) {
+      res.send(401).send({ message: 'not authentic user' })
+    } else {
+      Invoice.findById({ _id: req.params.id, status: true })
+        .then(invoice => {
+          res.status(200).send(invoice)
+        })
+        .catch(err => {
+          res.status(500).json(errorHandler(err))
+        })
+    }
+  })
 }
 
 module.exports.getInvoiceWithInvoiceDetails = async (req, res) => {
-  try {
-    let invoice = await Invoice.findOne({ _id: req.params.id })
-    let invoiceDetails = await InvoiceDetails.find({
-      invoiceId: req.params.id
-    })
-      .populate('itemId', 'name')
-      .populate('brandId', 'brandName')
+  jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (
+    err,
+    payload
+  ) {
+    if (err) {
+      res.send(401).send({ message: 'not authentic user' })
+    } else {
+      try {
+        let invoice = await Invoice.findOne({ _id: req.params.id })
+        let invoiceDetails = await InvoiceDetails.find({
+          invoiceId: req.params.id
+        })
+          .populate('itemId', 'name')
+          .populate('brandId', 'brandName')
 
-    let obj = { invoice, invoiceDetails }
-    res.status(200).send(obj)
-  } catch (err) {
-    res.status(500).send(errorHandler(err))
-  }
+        let obj = { invoice, invoiceDetails }
+        res.status(200).send(obj)
+      } catch (err) {
+        res.status(500).send(errorHandler(err))
+      }
+    }
+  })
 }
 
 module.exports.setInvoice = (req, res) => {
-  Invoice.create(req.body)
-    .then(invoice => {
-      res.status(200).send(invoice)
-    })
-    .catch(err => {
-      res.status(500).json(errorHandler(err))
-    })
+  jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (
+    err,
+    payload
+  ) {
+    if (err) {
+      res.send(401).send({ message: 'not authentic user' })
+    } else {
+      Invoice.create(req.body)
+        .then(invoice => {
+          res.status(200).send(invoice)
+        })
+        .catch(err => {
+          res.status(500).json(errorHandler(err))
+        })
+    }
+  })
 }
 
 module.exports.editInvoice = (req, res) => {
-  Invoice.findByIdAndUpdate({ _id: req.body._id, status: true }, req.body)
-    .then(() => {
-      Invoice.findById({ _id: req.body._id })
-        .then(invoice => {
-          res.status(200).send(invoice)
+  jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (
+    err,
+    payload
+  ) {
+    if (err) {
+      res.send(401).send({ message: 'not authentic user' })
+    } else {
+      Invoice.findByIdAndUpdate({ _id: req.body._id, status: true }, req.body)
+        .then(() => {
+          Invoice.findById({ _id: req.body._id })
+            .then(invoice => {
+              res.status(200).send(invoice)
+            })
+            .catch(err => {
+              res.status(500).json(errorHandler(err))
+            })
         })
         .catch(err => {
           res.status(500).json(errorHandler(err))
         })
-    })
-    .catch(err => {
-      res.status(500).json(errorHandler(err))
-    })
+    }
+  })
 }
 
 module.exports.deleteInvoice = (req, res) => {
-  Invoice.findByIdAndUpdate(
-    { _id: req.params.id, status: true },
-    { $set: { status: false } }
-  )
-    .then(() => {
-      Invoice.findById({ _id: req.params.id })
-        .then(invoice => {
-          res.status(200).send(invoice)
+  jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (
+    err,
+    payload
+  ) {
+    if (err) {
+      res.send(401).send({ message: 'not authentic user' })
+    } else {
+      Invoice.findByIdAndUpdate(
+        { _id: req.params.id, status: true },
+        { $set: { status: false } }
+      )
+        .then(() => {
+          Invoice.findById({ _id: req.params.id })
+            .then(invoice => {
+              res.status(200).send(invoice)
+            })
+            .catch(err => {
+              res.status(500).json(errorHandler(err))
+            })
         })
         .catch(err => {
           res.status(500).json(errorHandler(err))
         })
-    })
-    .catch(err => {
-      res.status(500).json(errorHandler(err))
-    })
+    }
+  })
 }
 
 module.exports.getCustomers = (req, res) => {
-  Customer.find({}, { _id: 1, clientName: 1 })
-    .then(customers => {
-      res.status(200).send(customers)
-    })
-    .catch(err => {
-      res.status(500).json(errorHandler(err))
-    })
+  jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (
+    err,
+    payload
+  ) {
+    if (err) {
+      res.send(401).send({ message: 'not authentic user' })
+    } else {
+      Customer.find({}, { _id: 1, clientName: 1 })
+        .then(customers => {
+          res.status(200).send(customers)
+        })
+        .catch(err => {
+          res.status(500).json(errorHandler(err))
+        })
+    }
+  })
 }
