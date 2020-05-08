5936190f0b07f77a2db6e1aadeccd16366b6a9af
hammadyounas <m.hammadyounas@outlook.com>
2020-05-08 12:56:07 +0500

    items api's set

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -2,6 +2,7 @@ const Items = require('../models/item.model')
 const jwt = require('jsonwebtoken')
 const History = require('../models/history.model')
 const historyController = require('./history.controller')
+const errorHandler = require('../utils/errorHandler')
 
 module.exports.getItems = (req, res) => {
   if (req.query.token != undefined) {
@@ -9,99 +10,93 @@ module.exports.getItems = (req, res) => {
       err,
       payload
     ) {
-      if (err) {
-        res.status(401).send({ message: 'not authentic user' })
-      } else {
         try{
+
+          if(err) return res.status(401).send({ message: 'not authentic user' })
           
           let list = await Items.find().exec();
           
-          if(!list.length) return res.status(404).send({msg:'Items Not Found'});
+          if(!list.length) return res.status(404).send({message:'Items Not Found'});
           
-          res.status(200).send(list);
+          return res.status(200).send(list);
   
         }catch(err){
-          res.status(500).send(err);
+          res.status(500).json(errorHandler(err))
         }
-      }
     })
   }else{
-    res.status(401).send({ message: 'not authentic user' });
+    return res.status(401).send({ message: 'not authentic user' });
   }
 }
 
 module.exports.deleteItems = (req, res) => {
   jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (err, payload) {
-    if (err) {
-      res.send(401).send({ message: 'not authentic user' })
-    } else {
       try{
           
-        let item = await  Items.remove({ _id: req.params.id });
+        if(err) return res.status(401).send({ message: 'not authentic user' })
+
+        await  Items.remove({ _id: req.params.id });
 
         let list = await Items.find().exec();
 
-        if(!list.length) return res.status(404).send({msg:'Items Not Found'});
+        if(!list.length) return res.status(404).send({message:'Items Not Found'});
         
         res.status(200).send(list);
 
       }catch(err){
-        res.status(500).send(err);
+        res.status(500).json(errorHandler(err))
       }
-    }
   })
 }
 
 module.exports.editItems = (req, res) => {
   jwt.verify(req.query.token, 'secretOfSasscoTraders',async function (err, payload) {
-    if (err) {
-      res.send(401).send({ message: 'not authentic user' })
-    } else {
-      try{        
+      try{  
+        
+        if(err) return res.status(401).send({ message: 'not authentic user' })
+
         let result = await  Items.findOne({ name: req.body.name })
 
-        if(result) return res.status(409).send({ msg: 'item already exist' });
+        if(result) return res.status(409).send({ message: 'item already exist' });
 
-        let editItem = await Items.findByIdAndUpdate({ _id: req.body._id },{ name: req.body.name },{ new: true })
+        await Items.findByIdAndUpdate({ _id: req.body._id },{ name: req.body.name },{ new: true })
 
-        let record = await historyController.addHistory(req.body.history,payload,'Item','update',0)
+        await historyController.addHistory(req.body.history,payload,'Item','update',0)
 
         let list = await Items.find().exec();
 
-        if(!list.length) return res.status(404).send({msg:'Items Not Found'});
+        if(!list.length) return res.status(404).send({message:'Items Not Found'});
         
         res.status(200).send(list);
 
       }catch(err){
-        res.status(500).send(err);
+        res.status(500).json(errorHandler(err))
       }
-    }
   })
 }
 
 module.exports.addItems = (req, res) => {
   jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (err, payload) {
-    if (err) {
-      res.send(401).send({ message: 'not authentic user' })
-    } else {
       try{
+
+        if(err) return res.status(401).send({ message: 'not authentic user' })
+
         let result = await Items.findOne({ name: req.body.name })
         
-        if(result) res.status(409).send({ msg: 'item already exist' });
+        if(result) res.status(409).send({ message: 'item already exist' });
 
-        let ninja = await Items.create(req.body);
+        await Items.create(req.body);
         
-        let record = await historyController.addHistory( req.body.history,payload,'item','add', 0)
+        await historyController.addHistory( req.body.history,payload,'item','add', 0)
         
         let list = await Items.find().exec();
         
-        if(!list.length) return res.status(404).send({msg:'items not found'})
+        if(!list.length) return res.status(404).send({message:'items not found'})
 
-        res.status(200).send(list)
+        return res.status(200).send(list)
 
       }catch(err){
-        res.status(500).send(err);
+        res.status(500).json(errorHandler(err))
       }
-    }
   })
 }
