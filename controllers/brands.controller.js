15b91391a16e8a5a75903f6026aebc4b70d57ed8
hammadyounas <m.hammadyounas@outlook.com>
2020-05-08 12:31:18 +0500

    brands api set

--- controllers/brands.controller.js
+++ controllers/brands.controller.js
@@ -3,45 +3,40 @@ const Items = require('../models/item.model')
 const utilFunction = require('../utils/ReplaceName')
 const historyController = require('./history.controller')
 const jwt = require('jsonwebtoken')
-
-let errorHandler = error => {
-  return {
-    stack: error.stack,
-    code: error.code,
-    message: error.message
-  }
-}
+const errorHandler = require('../utils/errorHandler')
 
 module.exports.getBrandsWithItems = async (req, res) => {
   jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (
     err,
     payload
   ) {
-    if (err) {
-      res.send(401).send({ message: 'not authentic user' })
-    } else {
-      try {
-        let brands = await Brands.find()
-        let arr = []
-        Promise.all(
-          brands.map(async obj => {
-            let item = await Items.findOne({ _id: obj.itemId })
-            let tempObj = {
-              brandName: obj.brandName,
-              _id: obj._id,
-              itemId: obj.itemId,
-              createdAt: obj.createdAt,
-              updatedAt: obj.updatedAt,
-              itemName: item.name
-            }
-            arr.push(tempObj)
-          })
-        ).then(result => {
-          res.status(200).send(arr)
+    try{
+
+      if(err) return res.send(401).send({ message: 'not authentic user' });
+
+      let brands = await Brands.find()
+      
+      let arr = []
+
+      await  Promise.all(
+        brands.map(async obj => {
+          let item = await Items.findOne({ _id: obj.itemId })
+          let tempObj = {
+            brandName: obj.brandName,
+            _id: obj._id,
+            itemId: obj.itemId,
+            createdAt: obj.createdAt,
+            updatedAt: obj.updatedAt,
+            itemName: item.name
+          }
+          arr.push(tempObj)
         })
-      } catch (err) {
-        res.status(500).send({ msg: 'internal server error' })
-      }
+      )
+
+      return res.status(200).send(arr)
+
+    }catch(err){
+      return res.status(500).json(errorHandler(error))
     }
   })
 }
@@ -51,10 +46,10 @@ module.exports.getBrands = async (req, res) => {
     err,
     payload
   ) {
-    if (err) {
-      // return res.send(401).send({ message: 'not authentic user' })
-    } else {
       try {
+        
+        if(err) return res.send(401).send({ message: 'not authentic user' });
+
         let brands = await Brands.find().populate('itemId','name');
 
         if(!brands.length) return res.status(404).send({ msg: 'No Data Found' })
@@ -62,11 +57,8 @@ module.exports.getBrands = async (req, res) => {
         return res.status(200).send(brands)
 
       } catch (err) {
-        return res.status(500).send({ msg: 'internal server error' })
+        return res.status(500).json(errorHandler(error))
       }
-    }
-  }).catch(err =>{
-    return res.send(401).send({ message: 'not authentic user' })
 
   })
 }
@@ -82,9 +74,10 @@ module.exports.addBrands = (req, res) => {
       try{
 
         let brands = req.body.brands;
+
         let addBrands = [];
 
-        Promise.all(
+        await Promise.all(
           brands.map(async obj => {
             let resp = await Brands.findOne({
               itemId: obj.itemId,
@@ -94,20 +87,20 @@ module.exports.addBrands = (req, res) => {
               addBrands.push(obj)
             }
           })
-        ).then(async result => {
+        )
               
-              let createBrands = await Brands.create(addBrands);
+        await Brands.create(addBrands);
 
-              let record = await historyController.addHistory(req.body.history,payload,'Brand','add',0);
+        await historyController.addHistory(req.body.history,payload,'Brand','add',0);
 
-              let brands = await Brands.find().populate('itemId','name');
+        let currentBrands = await Brands.find().populate('itemId','name');
 
-              if(!brands.length) return res.status(404).send({ msg: 'No Data Found' })
+        if(!currentBrands.length) return res.status(404).send({ msg: 'No Brands Data Found' });
           
-              return res.status(200).send(brands)
-        })
+        return res.status(200).send(currentBrands);
+
       }catch(err){
-        return res.status(500).send(err)
+        return res.status(500).json(errorHandler(error))
       }
      
       
@@ -120,51 +113,50 @@ module.exports.editBrands = async (req, res) => {
     err,
     payload
   ) {
-    if (err) {
-      res.send(401).send({ message: 'not authentic user' })
-    } else {
-      obj = req.body
-      let resp = await Brands.findOne({
-        itemId: obj.itemId,
-        brandName: obj.brandName
-      })
-      if (!resp) {
-        Brands.findByIdAndUpdate(
-          { _id: req.body._id },
-          { brandName: req.body.brandName, itemId: req.body.itemId._id },
-          { new: true }
-        ).exec(async (error, doc) => {
-          if (error) res.status(500).json(errorHandler(error))
-          let record = await historyController.addHistory(
-            req.body.history,
-            payload,
-            'Brand',
-            'update',
-            0
-          )
-          res.status(200).send(doc)
-        })
-      } else {
-        res
-          .status(409)
-          .send({ msg: 'conflict with same item name and brand name' })
-      }
+    try{
+
+      if(err) return  res.send(401).send({ message: 'not authentic user' })
+
+      let resp = await Brands.findOne({itemId: req.body.itemId,brandName: req.body.brandName});
+
+      if(resp) return res.status(409).send({ message: 'conflict with same item name and brand name' })
+
+      let doc = await Brands.findByIdAndUpdate(
+        { _id: req.body._id },
+        { brandName: req.body.brandName, itemId: req.body.itemId._id },
+        { new: true }
+      ).exec();
+
+      await historyController.addHistory(
+        req.body.history,
+        payload,
+        'Brand',
+        'update',
+        0
+      );
+
+      return res.status(200).send(doc)
+
+
+    }catch(err){
+      return res.status(500).json(errorHandler(error))
     }
   })
 }
 
 module.exports.deleteBrands = async (req, res) => {
-  jwt.verify(req.query.token, 'secretOfSasscoTraders', function (err, payload) {
-    if (err) {
-      res.send(401).send({ message: 'not authentic user' })
-    } else {
-      Brands.remove({ _id: req.params.id })
-        .then(brands => {
-          res.send(brands)
-        })
-        .catch(error => {
-          res.status(500).json(errorHandler(error))
-        })
+  jwt.verify(req.query.token, 'secretOfSasscoTraders', async function (err, payload) {
+    try{
+
+      if(err) return res.send(401).send({ message: 'not authentic user' })
+
+      let brands = await Brands.remove({ _id: req.params.id });
+
+      return res.status(200).send(brands)
+
+
+    }catch(err){
+      return res.status(500).json(errorHandler(error))
     }
   })
 }
@@ -174,22 +166,18 @@ module.exports.getItemBrands = async (req, res) => {
     err,
     payload
   ) {
-    if (err) {
-      res.send(401).send({ message: 'not authentic user' })
-    } else {
-      try {
-        let getItemBrands = await Brands.find({ itemId: req.body.itemId })
-        utilFunction
-          .replace(getItemBrands, Items)
-          .then(data => {
-            res.send(data)
-          })
-          .catch(error => {
-            res.status(500).json(errorHandler(error))
-          })
-      } catch (error) {
-        res.status(500).send(errorHandler(error))
-      }
+    try{
+
+      if(err) return res.send(401).send({ message: 'not authentic user' })
+
+      let getItemBrands = await Brands.find({ itemId: req.body.itemId })
+
+      let data = await utilFunction.replace(getItemBrands, Items);
+
+      return res.status(200).send(data);
+
+    }catch(err){
+      return res.status(500).send(errorHandler(error))
     }
   })
 }
