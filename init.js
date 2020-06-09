d6761f29ba9ff2e9076960bb409a93b46f792ed3
hammadyounas <m.hammadyounas@outlook.com>
2020-06-09 13:02:26 +0500

    port reset

--- init.js
+++ init.js
@@ -5,7 +5,7 @@ module.exports = () => {
   const cors = require('cors');
 
   const app = express()
-  const PORT = process.env.PORT || 3000
+  const PORT = process.env.PORT || 3001
 
   mongoose
     .connect(process.env.MONGO_URL, { useNewUrlParser: true,useUnifiedTopology: true })
